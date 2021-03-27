c13602dbbbee6697e89b60998bda3deb7d32223a
sphexoo <fabian_sauer@freenet.de>
2021-03-27 16:47:56 +0100

    Cleaned repo

--- switch.py
+++
@@ -1,9 +0,0 @@
-class Switch:
-  def __init__(self, serial, src, dst):
-    self.serial = serial
-    self.src = src
-    self.dst = dst
-
-  def toggle(self):
-      self.serial.write(self.src)
-      self.serial.write(self.dst)
